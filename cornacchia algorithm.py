8a480b8bc2865c11723b46f7694f95237a2413ec
haochenuw <chenh123@uw.edu>
2013-04-17 19:29:26 -0700

    Update cornacchia algorithm.py

--- cornacchia algorithm.py
+++ cornacchia algorithm.py
@@ -1,18 +1,19 @@
 def find_sol(p,d):
     r"""
     input - a prime p, an absolute value of discriminant d
-    output - a solution (x,y) in integers to the equation
+    output - a primitive solution (x,y) in integers to the equation
     x^2 + d*y^2 = p, if there exist one, otherwise return none.
     example:
     sage: find_sol(5,1) 
-    sage: (1,2)
+    sage: (2,1)
     """
     if kronecker(-d,p) == 1: 
         t = find_sqrt(p,-d)
-        bound = floor(p^(.5))
+        bound = p^(.5)
         n = p
-        while(t >= bound):
+        while True:
             n,t = t, n%t
+            if t < bound: break
         if ((p-t^2)/d)^(.5) in ZZ:
             return (t, ZZ(((p-t^2)/d)^(.5)))
         else:
