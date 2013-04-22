2a494ba8865db179ec34248ca6b419f25d70d049
haochenuw <chenh123@uw.edu>
2013-04-22 13:59:12 -0700

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -3,3 +3,15 @@ def sum_squares(n):
     for i in range(1,n+1):
         sum += i**2
     return sum
+    
+timeit('sum_squares(10000)')
+
+%cython
+def sum_squares1(int n):
+    result = []
+    cdef int sum, i = 0
+    for i in range(1,n+1):
+        sum += i**2
+    return sum
+    
+timeit('sum_squares1(10000)')
